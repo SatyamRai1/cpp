df7af4575345dae7653570c85f6ce4ca39e5b1bb
Satyam Rai <satyamr05@gmail.com>
2021-08-28 23:15:12 +0530

    Add files via upload

---
+++ using_variables.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName="John";
+    int characterAge=28;
+    
+    cout<<"There was once a cowboy named "<<characterName <<endl;
+    cout<<"He was "<<characterAge<<" Years old"<< endl;
+    cout<<"He also named his son "<<characterName << endl;
+    cout<<"But did not like being "<<characterAge<<" years old."<< endl;
+
+    return 0;
+}
