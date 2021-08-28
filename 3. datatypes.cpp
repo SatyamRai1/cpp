ed8b6280c476ec4d6a76ab2c84a6f049e78d07e7
Satyam Rai <satyamr05@gmail.com>
2021-08-28 23:23:57 +0530

    Add files via upload

---
+++ 3. datatypes.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade='A';
+    string characterName="John";
+    int characterAge=28;
+    double number= 23.5668;
+    float num2= 3.71; // less used; just use double
+    bool hasPassed= false;
+    
+    cout<< grade << endl;
+   
+    return 0;
+}
