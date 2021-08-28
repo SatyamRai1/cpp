9f0b38f9278df0e699ae5e72c4403655d3e54709
Satyam Rai <satyamr05@gmail.com>
2021-08-28 23:18:08 +0530

    Update 2. using_variables.cpp

--- 2. using_variables.cpp
+++ 2. using_variables.cpp
@@ -10,7 +10,7 @@ int main()
     
     cout<<"There was once a cowboy named "<<characterName <<endl;
     cout<<"He was "<<characterAge<<" Years old"<< endl;
-    characterName="Mike";
+    characterName="Mike";                                              // change the value stored in the variable halfway through the program
     cout<<"He also named his son "<<characterName << endl;
     cout<<"But did not like being "<<characterAge<<" years old."<< endl;
 
