bbbfa392f52b054d69dccbcd221af1c973904e55
Satyam Rai <satyamr05@gmail.com>
2021-08-28 23:17:24 +0530

    Update 2. using_variables.cpp

--- 2. using_variables.cpp
+++ 2. using_variables.cpp
@@ -10,6 +10,7 @@ int main()
     
     cout<<"There was once a cowboy named "<<characterName <<endl;
     cout<<"He was "<<characterAge<<" Years old"<< endl;
+    characterName="Mike";
     cout<<"He also named his son "<<characterName << endl;
     cout<<"But did not like being "<<characterAge<<" years old."<< endl;
 
